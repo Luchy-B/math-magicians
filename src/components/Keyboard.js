b4c60b216bd4d06d85deafb1f23dde670849169c
Luchy-B <okparabon@gmail.com>
2023-03-30 14:58:10 +0100

    Add event to project

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
+
+const obj = {
+  total: 0,
+  next: '',
+  operation: '',
+};
 
 function Keyboard() {
+  const [data, setData] = useState(obj);
+
+  const handleClick = (e) => {
+    setData(calculate(data, e.target.textContent));
+  };
+
   return (
-    <div>
-      <div className="keypad">
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="operator">&divide;</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="operator">&times;</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="operator">&ndash;</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="operator">+</button>
-        <button type="button" className="spanTwo">0</button>
-        <button type="button">.</button>
-        <button type="button" className="operator">=</button>
+    <>
+      <div className="container">
+        <div className="display">
+          <p>
+            {data.total}
+            {' '}
+            {data.operation}
+            {' '}
+            {data.next}
+          </p>
+        </div>
+
+        <div className="keypad">
+          <button type="button" onClick={handleClick}>AC</button>
+          <button type="button" onClick={handleClick}>+/-</button>
+          <button type="button" onClick={handleClick}>%</button>
+          <button type="button" className="operator" onClick={handleClick}>&divide;</button>
+          <button type="button" onClick={handleClick}>7</button>
+          <button type="button" onClick={handleClick}>8</button>
+          <button type="button" onClick={handleClick}>9</button>
+          <button type="button" className="operator" onClick={handleClick}>x</button>
+          <button type="button" onClick={handleClick}>4</button>
+          <button type="button" onClick={handleClick}>5</button>
+          <button type="button" onClick={handleClick}>6</button>
+          <button type="button" className="operator" onClick={handleClick}>-</button>
+          <button type="button" onClick={handleClick}>1</button>
+          <button type="button" onClick={handleClick}>2</button>
+          <button type="button" onClick={handleClick}>3</button>
+          <button type="button" className="operator" onClick={handleClick}>+</button>
+          <button type="button" className="spanTwo" onClick={handleClick}>0</button>
+          <button type="button" onClick={handleClick}>.</button>
+          <button type="button" className="operator" onClick={handleClick}>=</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 export default Keyboard;
